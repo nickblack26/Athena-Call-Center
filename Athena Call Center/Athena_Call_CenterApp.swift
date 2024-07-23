6e7933f48c42e56133a162a7528d452dc4d9979b
Nick Black <nickblack@VELO-V201849.local>
2024-07-23 08:18:45 -0500

    Initial Commit

---
+++ Athena Call Center/Athena_Call_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Athena_Call_CenterApp.swift
+//  Athena Call Center
+//
+//  Created by Nick Black on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Athena_Call_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athena Call Center/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Athena Call Center
+//
+//  Created by Nick Black on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
